eb6d9976c490cbce945db85c3fbb00160e74eb57
NjugunaKelvin <njugunak349@gmail.com>
2025-02-11 15:47:38 +0300

    implement cart addition functionality with AJAX support and update view names

--- cart/urls.py
+++ cart/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.summary,name='cart_summary'),
-    path('add/',views.add,name='cart_add'),
-    path('delete',views.delete,name='cart_delete'),
-    path('update/',views.update,name='cart_update'),
+    path('',views.cart_summary,name='cart_summary'),
+    path('add/',views.cart_add,name='cart_add'),
+    path('delete',views.cart_delete,name='cart_delete'),
+    path('update/',views.cart_update,name='cart_update'),
 ]
